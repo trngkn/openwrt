467206e070d4c40f5d0b09b43dfe30fea53c518a
trngkn <43349843+trngkn@users.noreply.github.com>
2021-10-29 09:58:11 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
